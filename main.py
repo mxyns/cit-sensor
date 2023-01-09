88a51b6246fc71f28f3994b10992a33531bc6c99
max <mx.yns@outlook.fr>
2023-01-09 12:05:44 +0100

    add time regulated loop + warning if can't match required frequency

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import sensor
 
 
 def main():
-
     parser = argparse.ArgumentParser(
         prog='SensorCIT',
         description='Capture images from a PiCamera and publish it to a MQTT Broker',
@@ -23,15 +22,21 @@ def main():
 
     with mqtt.MqttClient(config=mqtt_config) as client:
         with sensor.Camera() as camera:
-            image = camera.still_capture_sync(resize=(640, 360), save=camera_config["tmp_save_path"])
+            while True:
+                loop_start = time.time()
+                image = camera.still_capture_sync(resize=(640, 360), save=camera_config["tmp_save_path"])
 
-            buffered = BytesIO()
-            image.save(buffered, format="JPEG")
-            img_b64 = base64.b64encode(buffered.getvalue())
+                buffered = BytesIO()
+                image.save(buffered, format="JPEG")
+                img_b64 = base64.b64encode(buffered.getvalue())
 
-            client.publish(camera_config["topic"], img_b64.decode("ascii"))
+                client.publish(camera_config["topic"], img_b64.decode("ascii"))
 
-            time.sleep(camera_config["frequency"])
+                loop_duration = time.time() - loop_start
+                need_to_sleep = camera_config["frequency"] - loop_duration
+                time.sleep(max(0, need_to_sleep))
+                if need_to_sleep <= 0:
+                    print(f"Warning, capture is taking too long to match the configured frequency of {camera_config['frequency']}s")
 
 
 if __name__ == '__main__':
